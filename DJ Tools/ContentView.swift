d104d297e948847787f544910e66b89cc35c66de
JackPadalino <pada0867@gmail.com>
2023-12-18 12:23:23 -0500

    Initial Commit

---
+++ DJ Tools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DJ Tools
+//
+//  Created by Jack Padalino on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DJ Tools/DJ_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJ_ToolsApp.swift
+//  DJ Tools
+//
+//  Created by Jack Padalino on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DJ_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
